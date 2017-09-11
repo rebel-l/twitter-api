fe08b28ee467bc695496154aeaccdaecc1ef9ac8
Rebel-L <dj@rebel-l.net>
2017-09-12 00:59:55 +0200

    finished rendering

--- public/app.js
+++ public/app.js
@@ -13,6 +13,43 @@ $(document).ready(function () {
     });
 });
 
+var Renderer = {
+    getTotal: function(count) {
+        var total = document.createElement('h2');
+        total.innerHTML = 'Treffer insgesamt: ' + count;
+        return total;
+    },
+    getTweetHeadline: function() {
+        var headline = document.createElement('h2');
+        headline.innerHTML = 'Gefundene Tweets:';
+        return headline;
+    },
+    getHit: function(hit) {
+        console.log(hit._source.text);
+        var li = document.createElement('li');
+        li.innerHTML = hit._source.text + ' (' + hit._source.createdAt + ')';
+        return li;
+    },
+    getHits: function (hits) {
+        var list = document.createElement('ol');
+        var i, len = hits.length;
+
+        for(i = 0; i < len; i++) {
+            list.appendChild(this.getHit(hits[i]))
+        }
+        return list;
+    }
+};
+
 function success(data) {
-    console.log(data);
+    console.log(data.hits[0]);
+
+    var resultDom = $('#result')[0];
+    resultDom.innerHTML = '';
+    resultDom.append(Renderer.getTotal(data.total));
+    if(data.total > 0) {
+        resultDom.append(Renderer.getTweetHeadline());
+        resultDom.append(Renderer.getHits(data.hits));
+    }
 }
+
